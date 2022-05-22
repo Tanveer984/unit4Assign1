ce7673ca24a58ca3149b29b09a99711258a41aeb
Tanveer984 <mohdtanveer984@gmail.com>
2022-05-22 23:40:00 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <>
-    <div className="main">
-    <h2>
-        Mobile operating System</h2>
-    <li>IOS</li>
-    <li>Android</li>
-    <li>Blackbarry</li>
-    <li>Windows</li>
-    <h2>Mobile Manufacturers</h2>
-    <li>Apple</li>
-    <li>Samsung</li>
-    <li>Blackbarry</li>
-    <li>Microsoft</li>
-    </div>
-    </>
+    <Card />
+  </>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div className="main">
+    <h2>
+        Mobile operating System</h2>
+    <li>IOS</li>
+    <li>Android</li>
+    <li>Blackbarry</li>
+    <li>Windows</li>
+    <h2>Mobile Manufacturers</h2>
+    <li>Apple</li>
+    <li>Samsung</li>
+    <li>Blackbarry</li>
+    <li>Microsoft</li>
+    </div>
+  )
+}
